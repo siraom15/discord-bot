4439bd2e7a9182876372979598f2d5b7b2518e7c
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-09-18 12:17:37 +0700

    refactor

--- src/function/getInviteLink.js
+++ src/function/getInviteLink.js
@@ -1,3 +1,3 @@
-let getInviteLink = (message, client_id) => message.channel.send(`Link เชิญบอท : https://discord.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=8`);
+let getInviteLink = (message, client_id) => message.channel.send(`\`\`\`🔗Link เชิญบอท : https://discord.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=8\`\`\``);
 
-module.exports =getInviteLink ;
+module.exports = getInviteLink;
