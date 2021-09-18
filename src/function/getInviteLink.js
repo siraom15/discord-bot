4b529ade0977a96e477d1a1d7ba80330194c3399
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-09-19 02:06:25 +0700

    refactor & add commonText

--- src/function/getInviteLink.js
+++ src/function/getInviteLink.js
@@ -3,11 +3,7 @@ const Discord = require('discord.js');
 let getInviteLink = (message, client_id) => {
     let url = `https://discord.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=8`;
 
-    const button = new Discord.MessageButton()
-        .setLabel('LINK')
-        .setStyle('LINK')
-        .url(url)
-    return message.reply(`\`\`\`🔗Link เชิญบอท🔗 https://discord.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=8\`\`\``, { button: button });
+    return message.reply(`\`\`\`🔗Link เชิญบอท🔗\`\`\`${url}`);
 }
 
 module.exports = getInviteLink;
