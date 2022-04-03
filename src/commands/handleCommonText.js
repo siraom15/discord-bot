089efe0c0443aa4711afb99c8703ca376520016f
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-03 21:45:36 +0700

    Add chance of occur common text

--- src/commands/handleCommonText.js
+++ src/commands/handleCommonText.js
@@ -1,9 +1,13 @@
-let commonText = require("../data/commonText")
-
+let commonText = require("../data/commonText");
+const random = require("../utils/random");
 let handleCommonText = (message) => {
-    for (const m in commonText)
-        if (message.content.includes(m))
-            return message.channel.send(commonText[m]);
-}
+  for (const m in commonText) {
+    if (message.content.includes(m)) {
+      if (random(0, 1) === 0) {
+        return message.channel.send(commonText[m]);
+      }
+    }
+  }
+};
 
 module.exports = handleCommonText;
